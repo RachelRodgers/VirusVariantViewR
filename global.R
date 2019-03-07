74b99fea60d4790ec1db4876e6c070810339c68a
RachelRodgers <rachelrodgers@wustl.edu>
2019-03-07 15:38:30 -0600

    Modifications to common variant functions.
    Changed the "Variant" class to hold a data frame of information about each variant to be displayed in the common variant table.  Made changes to BuildSampleObjects() and FindExactMatches() to incorporate this additional information.  Still need to put in a way to handle the search for exact matches when all the selected samples have no data in their VCF files.

--- global.R
+++ global.R
@@ -20,11 +20,8 @@ setClass("Variant",
          representation = representation(parent_sample = "character",
                                          position = "numeric",
                                          ref_allele = "character",
-                                         alt_allele = "character"),
-         prototype = prototype(parent_sample = NA_character_,
-                               position = NA_real_,
-                               ref_allele = NA_character_,
-                               alt_allele = NA_character_))
+                                         alt_allele = "character",
+                                         variant_df = "data.frame"))
 
 #----- Function Definitions -----#
 
@@ -37,10 +34,12 @@ setClass("Variant",
 #dataSetSamples <- as.character(testDataSet$Sample) # send to sampleVector
 
 BuildSampleObjects <- function(dataSet, sampleVector) {
+  
   # dataSet will come from input$dataSetSelect
   # sampleVector should automatically populate with all the samples available
   #   in the selected data set
   sampleClassList <- vector(mode = "list", length = length(sampleVector))
+  
   # Build a Sample class object from the vector
   for (i in 1:length(sampleVector)) {
     currentSample <- sampleVector[i]
@@ -48,31 +47,42 @@ BuildSampleObjects <- function(dataSet, sampleVector) {
     currentVCFFile <- GetVCF(dataSet, sample = currentSample)
     # Stop here if nothing is in the sample
     if (nrow(currentVCFFile) == 0) {
-      warning(paste("No variants detected in sample", currentSample),
+      warning(paste(currentSample, "has no variants detected"),
               call. = FALSE)
       newSample <- new("Sample",
                        sample_name = currentSample,
                        variant_list = list(0))
       sampleClassList[[i]] <- newSample
       names(sampleClassList)[i] <- currentSample
-      #sampleClassList[[currentSample]] <- newSample
     } else {
       currentSampleVariantList <- vector(mode = "list", length = nrow(currentVCFFile))
+      
       for (j in 1:nrow(currentVCFFile)) {
         positionString <- as.character(currentVCFFile[j, "Position"])
         referenceString <- as.character(currentVCFFile[j, "Reference"])
         alternativeString <- currentVCFFile[j, "Alternative"]
         variantID <- paste(positionString, referenceString, alternativeString,
                            sep = "_")
+        variantReadableID <- paste(positionString, " ", referenceString, "/", alternativeString, sep = "")
+        variantDF <- data.frame("Variation" = variantID,
+                                "Variation_Readable" = variantReadableID,
+                                "Reference_Allele" = referenceString,
+                                "Reference_Codon" = currentVCFFile[j, "Reference Codon"],
+                                "Reference_Protein" = currentVCFFile[j, "Reference Protein"],
+                                "Mutant_Allele" = alternativeString,
+                                "Mutant_Codon" = currentVCFFile[j, "Mutant Codon"],
+                                "Mutant_Protein" = currentVCFFile[j, "Mutant Protein"],
+                                "Mutation_Type" = currentVCFFile[j, "Mutation Type"])
         newVariant <- new("Variant",
                           parent_sample = currentSample,
                           position = as.numeric(positionString),
                           ref_allele = referenceString,
-                          alt_allele = alternativeString)
+                          alt_allele = alternativeString,
+                          variant_df = variantDF)
         currentSampleVariantList[[j]] <- newVariant
         names(currentSampleVariantList)[j] <- variantID
-        #currentSampleVariantList[[variantID]] <- newVariant
       }
+      
       # Fill in the sample object
       newSample <- new("Sample",
                        sample_name = currentSample,
@@ -82,9 +92,11 @@ BuildSampleObjects <- function(dataSet, sampleVector) {
       names(sampleClassList)[i] <- currentSample
       #sampleClassList[[currentSample]] <- newSample
     }
+    
   } 
   return(sampleClassList)
 }
+
 
 #----- Set up Sample Data Table -----#
 
@@ -229,6 +241,9 @@ PlotCoverage <- function(dataSet, sample, positions = NULL, widths = 1) {
 }
 
 #----- Build Common Variants Table (Exact Matching) -----#
+
+# Need to handle condition where there's no data!
+
 FindExactMatches <- function(sampleObjectList) {
   # Check if any samples have 0 variants and remove.  
   variantsAreNull <- vector(mode = "logical", length = length(sampleObjectList))
@@ -242,31 +257,50 @@ FindExactMatches <- function(sampleObjectList) {
   
   # For all the variants between the selected samples, make a table showing which
   #   samples they are in.
-  #   First check that the sample has variants. (is it's variant_list slot == 0?)
-  #   If yes, skip it.
-  variantDFList <- vector(mode = "list", length = length(selectedSampleObjectList))
+  
+  sampleVariantsDFList <- vector(mode = "list", length = length(selectedSampleObjectList))
+  sampleVariantsPresenceList <- vector(mode = "list", length = length(selectedSampleObjectList))
+  # pull extra info here and stick into currentSampleDF
   for (k in 1:length(selectedSampleObjectList)) {
+    # Get current sample information
     currentSample <- selectedSampleObjectList[[k]]
     currentSampleName <- currentSample@sample_name
-    currentSampleDF <- data.frame(names(currentSample@variant_list), 1)
-    colnames(currentSampleDF) <- c("variants", currentSampleName)
-    variantDFList[[k]] <- currentSampleDF
+    
+    # Build concatenated data frame from all the variants' variant_df slots
+    #   get current Variants, put in list
+    currentVariantList <- currentSample@variant_list
+    #   pull out each variant's variant_df, put in list
+    currentVariantDFList <- map(.x = seq(1:length(currentVariantList)),
+                                .f = function(idx) currentVariantList[[idx]]@variant_df)
+    #   reduce the list into one big df - (all variants' info for one sample only)
+    combinedVariantDFs <- Reduce(f = function(df1, df2) {rbind(x = df1, y = df2)},
+                                 x = currentVariantDFList)
+    
+    #   store the samples variants info in sampleVariantsDFList to be reduced later
+    sampleVariantsDFList[[k]] <- combinedVariantDFs
+    
+    # Build variantPresenceDF that will be used for searching for common variants
+    variantPresenceDF <- data.frame(names(currentSample@variant_list), 1)
+    colnames(variantPresenceDF) <- c("variants", currentSampleName)
+    sampleVariantsPresenceList[[k]] <- variantPresenceDF
   }
   
-  # Merge the variant data frames together, convert NA's to 0's
-  fullDF <- Reduce(f = function(df1, df2) {merge(x = df1, y = df2,
-                                                 by = "variants", all = TRUE)},
-                   x = variantDFList)
-  fullDF[is.na(fullDF)] <- 0
+  # Merge the presence data frames together, convert NA's to 0's
+  fullPresenceDF <- Reduce(f = function(df1, df2) {merge(x = df1, y = df2,
+                                                         by = "variants", all = TRUE)},
+                           x = sampleVariantsPresenceList)
+  fullPresenceDF[is.na(fullPresenceDF)] <- 0
+  
+  # remove extra columns before attempting rowSums
   
   # What are the counts for each variant?
-  fullDFModified <- column_to_rownames(fullDF, var = "variants")
-  variantCounts <- rowSums(fullDFModified)
+  fullPresenceDFModified <- column_to_rownames(fullPresenceDF, var = "variants")
+  variantCounts <- rowSums(fullPresenceDFModified)
   
   # Which samples do you find each variant in?
-  variantInSamples <- vector(mode = "character", length = nrow(fullDFModified))
-  for (m in 1:nrow(fullDFModified)) {
-    currentRow <- fullDFModified[m, ]
+  variantInSamples <- vector(mode = "character", length = nrow(fullPresenceDFModified))
+  for (m in 1:nrow(fullPresenceDFModified)) {
+    currentRow <- fullPresenceDFModified[m, ]
     currentVariantName <- rownames(currentRow)
     samplesVecString <- paste(colnames(currentRow)[currentRow != 0], collapse = ", ")
     variantInSamples[m] <- samplesVecString
@@ -282,15 +316,33 @@ FindExactMatches <- function(sampleObjectList) {
          call. = FALSE)
   }
   # Otherwise, merge them.
-  variantDataDF <- merge(variantInSamplesDF, variantCountsDF, by = "row.names")
+  variantDataDF <- merge(variantCountsDF, variantInSamplesDF, by = "row.names")
   variantDataDF <- variantDataDF %>%
     filter(Number_of_Samples >= 2) %>%
     arrange(desc(Number_of_Samples)) %>%
-    column_to_rownames(var = "Row.names")
+    column_to_rownames(var = "Row.names") %>%
+    rownames_to_column("Variation")
   
   # Put warning within app.R to deal with empty table.
   
-  return(variantDataDF)
+  # Now pull extra information before displaying table - from Reducing information 
+  #   from sampleVariantsDFList
+  variantInformationDF <- Reduce(f = function(df1, df2) {merge(x = df1, y = df2, all = TRUE)},
+                                 x = sampleVariantsDFList)
+  
+  # From variantInformationDF, append information to variantDataDF
+  variantInformationFullDF <- merge(x = variantDataDF, variantInformationDF, 
+                                    by = "Variation", all = FALSE)
+  
+  # Modify the table
+  variantInformationFinal <- variantInformationFullDF %>%
+    select(-c(Variation)) %>%
+    select(Variation_Readable, everything()) %>%
+    data.table::setnames(old = "Variation_Readable",
+                         new = "Variation") %>%
+    arrange(desc(Number_of_Samples))
+  
+  return(variantInformationFinal)
 }
 
 
